1b34abc251d49384eb383afe9e9bfcf365480f34
Pier-Hugues Pellerin <phpellerin@gmail.com>
2025-01-18 10:31:38 -0500

    Adding initial methods for the framebuffer

---
+++ src/framebuffer/mod.rs
@@ -0,0 +1,104 @@
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Cell {
+    character: char,
+}
+
+impl Cell {
+    pub fn new(character: char) -> Self {
+        Self { character }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Framebuffer {
+    width: u16,
+    height: u16,
+    buf: Vec<Option<Cell>>,
+}
+
+impl Framebuffer {
+    pub fn new(width: u16, height: u16) -> Self {
+        let capacity = width * height;
+        let buf = vec![None; capacity.into()];
+
+        Self { width, height, buf }
+    }
+
+    fn idx(&self, x: u16, y: u16) -> usize {
+        (x * self.width + y).into()
+    }
+
+    pub fn set(&mut self, x: u16, y: u16, cell: Cell) {
+        assert!(x < self.width, "X is out of bound of {}", self.width);
+        assert!(y < self.height, "Y is out of bounf of {}", self.height);
+
+        self.buf.insert(self.idx(x, y), Some(cell))
+    }
+
+    pub fn get(&self, x: u16, y: u16) -> Option<&Cell> {
+        assert!(x < self.width, "X is out of bound of {}", self.width);
+        assert!(y < self.height, "Y is out of bounf of {}", self.height);
+
+        match self.buf.get(self.idx(x, y)) {
+            Some(o) => o.as_ref(),
+            None => None,
+        }
+    }
+
+    #[allow(unused)]
+    pub fn resize(&mut self, _x: u16, _y: u16) {
+        unimplemented!()
+    }
+
+    #[allow(unused)]
+    pub fn clear(&mut self) {
+        self.buf.clear()
+    }
+
+    #[allow(unused)]
+    pub fn capacity(&self) -> usize {
+        (self.width * self.height).into()
+    }
+
+    #[allow(unused)]
+    pub fn width(&self) -> u16 {
+        self.width
+    }
+
+    #[allow(unused)]
+    pub fn height(&self) -> u16 {
+        self.height
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn set_and_get() {
+        let mut fb = Framebuffer::new(3, 4);
+        let cell = Cell { character: 'a' };
+        fb.set(1, 1, cell.clone());
+        let c = fb.get(1, 1).unwrap();
+        assert_eq!(cell, *c);
+    }
+
+    #[test]
+    fn capacity() {
+        let fb = Framebuffer::new(3, 4);
+        assert_eq!(fb.capacity(), 12);
+    }
+
+    #[test]
+    fn clear() {
+        let mut fb = Framebuffer::new(3, 4);
+        let cell = Cell { character: 'a' };
+        fb.set(1, 1, cell.clone());
+        let c = fb.get(1, 1).unwrap();
+        assert_eq!(cell, *c);
+
+        fb.clear();
+        assert!(fb.get(1, 2).is_none());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use framebuffer::{Cell, Framebuffer};
+
+mod framebuffer;
+
 fn main() {
-    println!("Hello, world!");
+    let mut fb = Framebuffer::new(10, 20);
+
+    fb.set(2, 3, Cell::new('P'));
+    fb.set(2, 4, Cell::new('H'));
+    fb.set(2, 5, Cell::new('!'));
 }
