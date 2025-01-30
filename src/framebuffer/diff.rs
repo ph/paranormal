a680bc2f58a8a950d15842c98801f5f812e85749
Pier-Hugues Pellerin <phpellerin@gmail.com>
2025-01-30 14:10:06 -0500

    Fix iterator()

--- src/framebuffer/diff.rs
+++ src/framebuffer/diff.rs
@@ -19,7 +19,7 @@ impl std::fmt::Display for Changeset {
 
 type Changesets = Vec<Changeset>;
 
-pub fn compare(a: Framebuffer, b: Framebuffer) -> Changesets {
+pub fn compare(a: &Framebuffer, b: &Framebuffer) -> Changesets {
     let mut changesets = Vec::new();
 
     a.iter()
@@ -59,4 +59,41 @@ pub fn compare(a: Framebuffer, b: Framebuffer) -> Changesets {
             };
         });
     Vec::new()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::framebuffer::render;
+
+    #[test]
+    fn compare_same_framebuffer() {
+        let mut fb = Framebuffer::new(2, 4);
+
+        fb.set(0, 0, Cell::Filled { character: 'H' });
+        fb.set(1, 1, Cell::Filled { character: 'P' });
+
+        let diff = compare(&fb, &fb.clone());
+
+        // let mut buf = String::new();
+        // render(&fb, &mut buf).expect("ok");
+
+        // assert_eq!(buf, "");
+
+        assert_eq!(diff.len(), 0, "raw diff {:?}", diff);
+    }
+
+    // fn compare_changed_framebuffer() {
+    //     let mut fb_a = Framebuffer::new(2, 4);
+
+    //     fb_a.set(2, 3, Cell::Filled { character: 'P' });
+    //     fb_a.set(2, 2, Cell::Filled { character: 'H' });
+
+    //     let mut fb_b = Framebuffer::new(2, 4);
+
+    //     fb_b.set(2, 2, Cell::Filled { character: '!' });
+    //     fb_b.set(0, 2, Cell::Filled { character: 'A' });
+
+    //     assert_eq!(compare(fb_a, fb_b).len(), 3);
+    // }
 }
--- src/framebuffer/mod.rs
+++ src/framebuffer/mod.rs
@@ -1,4 +1,21 @@
+use std::fmt::Write;
+
 mod diff;
+
+#[derive(Debug, Clone)]
+pub enum FramebufferErr {
+    Writing(String),
+}
+
+impl std::error::Error for FramebufferErr {}
+
+impl std::fmt::Display for FramebufferErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FramebufferErr::Writing(err) => write!(f, "can't write to buffer, error: {}", err),
+        }
+    }
+}
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Cell {
@@ -28,15 +45,35 @@ impl Framebuffer {
     }
 
     pub fn set(&mut self, x: u16, y: u16, cell: Cell) {
-        assert!(x < self.width, "X is out of bound of {}", self.width);
-        assert!(y < self.height, "Y is out of bounf of {}", self.height);
+        assert!(
+            x < self.width,
+            "X value of {} is out of bound of {}",
+            x,
+            self.width
+        );
+        assert!(
+            y < self.height,
+            "Y value of {} is out of bounf of {}",
+            y,
+            self.height
+        );
 
         self.buf.insert(self.idx(x, y), cell)
     }
 
     pub fn get(&self, x: u16, y: u16) -> &Cell {
-        assert!(x < self.width, "X is out of bound of {}", self.width);
-        assert!(y < self.height, "Y is out of bounf of {}", self.height);
+        assert!(
+            x < self.width,
+            "X value of {} is out of bound of {}",
+            x,
+            self.width
+        );
+        assert!(
+            y < self.height,
+            "Y value of {} is out of bounf of {}",
+            y,
+            self.height
+        );
 
         match self.buf.get(self.idx(x, y)) {
             Some(cell) => cell,
@@ -76,13 +113,12 @@ impl Framebuffer {
 
 pub struct FramebufferIterator<'a> {
     fb: &'a Framebuffer,
-    x: u16,
-    y: u16,
+    idx: usize,
 }
 
 impl<'a> FramebufferIterator<'a> {
     fn new(fb: &'a Framebuffer) -> Self {
-        Self { fb: fb, x: 0, y: 0 }
+        Self { fb: fb, idx: 0 }
     }
 }
 
@@ -90,21 +126,45 @@ impl<'a> Iterator for FramebufferIterator<'a> {
     type Item = ((u16, u16), &'a Cell);
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.y >= self.fb.width() {
-            self.y = 0;
-            self.x += 1;
-        }
-
-        if self.x >= self.fb.height() {
+        if self.idx == self.fb.capacity() {
             return None;
         }
 
-        let cell = ((self.x, self.y), self.fb.get(self.x, self.y));
+        let x: u16 = (self.idx % self.fb.width() as usize)
+            .try_into()
+            .expect("X position is out of bound");
 
-        self.y += 1;
+        let y: u16 = (self.idx / self.fb.width() as usize)
+            .try_into()
+            .expect(" Y position is out of bound");
+
+        let cell = ((x, y), self.fb.get(x, y));
+
+        self.idx += 1;
 
         Some(cell)
     }
+}
+
+pub fn render<W: Write>(fb: &Framebuffer, out: &mut W) -> Result<(), FramebufferErr> {
+    let mut i = 0;
+
+    for ((_x, _y), cell) in fb.iter() {
+        i += 1;
+
+        match cell {
+            Cell::Empty => write!(out, " ").map_err(|e| FramebufferErr::Writing(e.to_string()))?,
+            Cell::Filled { character } => {
+                write!(out, "{}", character).map_err(|e| FramebufferErr::Writing(e.to_string()))?
+            }
+        }
+
+        if i % fb.width() == 0 {
+            write!(out, "\n").map_err(|e| FramebufferErr::Writing(e.to_string()))?;
+        }
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -162,11 +222,24 @@ mod test {
 
         let expected = vec![
             ((0, 0), &cell_1),
-            ((0, 1), &Cell::Empty),
             ((1, 0), &Cell::Empty),
+            ((0, 1), &Cell::Empty),
             ((1, 1), &cell_2),
         ];
 
         assert_eq!(fb.iter().collect::<Vec<_>>(), expected);
+    }
+
+    #[test]
+    fn render_to_raw_buffer() {
+        let mut fb = Framebuffer::new(2, 2);
+        fb.set(0, 0, Cell::Filled { character: 'X' });
+        fb.set(1, 1, Cell::Filled { character: 'Y' });
+
+        let mut buf = String::new();
+
+        render(&fb, &mut buf).expect("should be able to write to the buffer");
+
+        assert_eq!(buf, String::from("X \n Y\n"));
     }
 }
