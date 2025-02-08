a961c3f8588cf58b4828455779e76b758c6331ca
Pier-Hugues Pellerin <phpellerin@gmail.com>
2025-02-07 21:07:51 -0500

    Write the appropriate escape code to write colors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use framebuffer::{Cell, Framebuffer};
-
 mod framebuffer;
+mod renderer;
+mod terminal;
+
+use framebuffer::{Cell, Framebuffer};
+use terminal::{Color, Command, Style};
 
 fn main() {
     let mut fb = Framebuffer::new(10, 20);
@@ -8,4 +11,22 @@ fn main() {
     fb.set(2, 3, Cell::Filled { character: 'P' });
     fb.set(2, 4, Cell::Filled { character: 'H' });
     fb.set(2, 5, Cell::Filled { character: '!' });
+
+    use Command::*;
+    use Style::*;
+
+    let a = vec![
+        Clear,
+        MoveTo(2, 2),
+        ApplyStyle(Background(Color::Red)),
+        ApplyStyle(Foreground(Color::Yellow)),
+        Write("Wooo"),
+        ApplyStyle(Reset),
+        ApplyStyle(Foreground(Color::Cyan)),
+        Write(" YES"),
+    ];
+
+    for i in a {
+        print!("{}", i);
+    }
 }
--- src/terminal/mod.rs
+++ src/terminal/mod.rs
@@ -1,7 +1,10 @@
+use std::borrow::Cow;
+
 #[derive(Debug, Clone)]
 pub enum Style {
     Foreground(Color),
     Background(Color),
+    Reset,
 }
 
 impl std::fmt::Display for Style {
@@ -10,13 +13,44 @@ impl std::fmt::Display for Style {
         match self {
             Foreground(color) => match color {
                 Color::Rgb { .. } => write!(f, "\x1B[38;2{}m", color.fg()),
-                _ => write!(f, "\x1B[38:5{}m]"),
+                _ => write!(f, "\x1B[38;{}m", color.fg()),
             },
 
             Background(color) => match color {
                 Color::Rgb { .. } => write!(f, "\x1B[48;2{}m", color.fg()),
-                _ => write!(f, "\x1B[48:5{}m]"),
+                _ => write!(f, "\x1B[48;{}m", color.bg()),
             },
+            Reset => write!(f, "\x1B[0m"),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Visibility {
+    Hidden,
+    Show,
+}
+
+#[derive(Debug, Clone)]
+pub enum Command<'a> {
+    MoveTo(u16, u16),
+    ApplyStyle(Style),
+    Write(&'a str),
+    Cursor(Visibility),
+    Clear,
+}
+
+impl std::fmt::Display for Command<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Command::MoveTo(x, y) => write!(f, "\x1B[{};{}H", x, y),
+            Command::ApplyStyle(style) => write!(f, "{}", style),
+            Command::Write(s) => write!(f, "{}", s),
+            Command::Cursor(visibility) => match visibility {
+                Visibility::Hidden => write!(f, "{}", "\x1B[?25l"),
+                Visibility::Show => write!(f, "{}", "\x1B[?25h"),
+            },
+            Command::Clear => write!(f, "{}", "\x1B[2J"),
         }
     }
 }
@@ -44,49 +78,49 @@ pub enum Color {
 }
 
 impl Color {
-    pub fn bg(&self) -> &'static str {
+    pub fn bg(&self) -> Cow<str> {
         use Color::*;
         match self {
-            Rgb { r, g, b } => &format!("{};{};{}", r, g, b),
-            Black => "40",
-            Red => "41",
-            Green => "42",
-            Yellow => "43",
-            Blue => "44",
-            Magenta => "45",
-            Cyan => "46",
-            White => "47",
-            BrightBlack => "100",
-            BrightRed => "101",
-            BrightGreen => "102",
-            BrightYellow => "103",
-            BrightBlue => "104",
-            BrightMagenta => "105",
-            BrightCyan => "106",
-            BrightWhite => "107",
+            Rgb { r, g, b } => Cow::Owned(format!("{};{};{}", r, g, b)),
+            Black => Cow::Borrowed("40"),
+            Red => Cow::Borrowed("41"),
+            Green => Cow::Borrowed("42"),
+            Yellow => Cow::Borrowed("43"),
+            Blue => Cow::Borrowed("44"),
+            Magenta => Cow::Borrowed("45"),
+            Cyan => Cow::Borrowed("46"),
+            White => Cow::Borrowed("47"),
+            BrightBlack => Cow::Borrowed("100"),
+            BrightRed => Cow::Borrowed("101"),
+            BrightGreen => Cow::Borrowed("102"),
+            BrightYellow => Cow::Borrowed("103"),
+            BrightBlue => Cow::Borrowed("104"),
+            BrightMagenta => Cow::Borrowed("105"),
+            BrightCyan => Cow::Borrowed("106"),
+            BrightWhite => Cow::Borrowed("107"),
         }
     }
 
-    pub fn fg(&self) -> &'static str {
+    pub fn fg(&self) -> Cow<str> {
         use Color::*;
         match self {
-            Rgb { r, g, b } => &format!("{};{};{}", r, g, b),
-            Black => "30",
-            Red => "31",
-            Green => "32",
-            Yellow => "33",
-            Blue => "34",
-            Magenta => "35",
-            Cyan => "36",
-            White => "37",
-            BrightBlack => "90",
-            BrightRed => "91",
-            BrightGreen => "92",
-            BrightYellow => "93",
-            BrightBlue => "94",
-            BrightMagenta => "95",
-            BrightCyan => "96",
-            BrightWhite => "97",
+            Rgb { r, g, b } => Cow::Owned(format!("{};{};{}", r, g, b)),
+            Black => Cow::Borrowed("30"),
+            Red => Cow::Borrowed("31"),
+            Green => Cow::Borrowed("32"),
+            Yellow => Cow::Borrowed("33"),
+            Blue => Cow::Borrowed("34"),
+            Magenta => Cow::Borrowed("35"),
+            Cyan => Cow::Borrowed("36"),
+            White => Cow::Borrowed("37"),
+            BrightBlack => Cow::Borrowed("90"),
+            BrightRed => Cow::Borrowed("91"),
+            BrightGreen => Cow::Borrowed("92"),
+            BrightYellow => Cow::Borrowed("93"),
+            BrightBlue => Cow::Borrowed("94"),
+            BrightMagenta => Cow::Borrowed("95"),
+            BrightCyan => Cow::Borrowed("96"),
+            BrightWhite => Cow::Borrowed("97"),
         }
     }
 }
