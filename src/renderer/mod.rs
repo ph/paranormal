964fd7c1cedcf1215dd31b2f9e413192f306396a
ph <phpellerin@gmail.com>
2025-04-22 15:55:00 -0400

    bug: return a slice instead of a Vec<Command>

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -39,23 +39,19 @@ impl<T: Write> Terminal<T> {
     fn update(&mut self, x: u16, y: u16, cell: Cell) {
         use Command::*;
 
-        let ops = match cell {
-            Cell::Empty => empty_at(x, y),
+        match cell {
+            Cell::Empty => self.apply(&empty_at(x, y)),
             Cell::Filled {
                 character,
                 foreground,
                 background,
             } => {
-                vec![
-                    MoveTo(x, y),
-                    ApplyStyle(foreground),
-                    ApplyStyle(background),
-                    Write(character.to_string()),
-                ]
+		self.apply(&[MoveTo(x, y),
+			     ApplyStyle(foreground),
+			     ApplyStyle(background),
+			     Write(character.to_string())])
             }
         };
-
-        self.apply(&ops)
     }
 
     fn apply(&mut self, ops: &[Command]) {
@@ -65,14 +61,12 @@ impl<T: Write> Terminal<T> {
     }
 }
 
-fn empty_at(x:  u16, y: u16) -> Vec<Command> {
+fn empty_at(x:  u16, y: u16) -> [Command; 3] {
     use Command::*;
     
-    vec![
-	MoveTo(x, y),
-	ApplyStyle(Style::Reset),
-	Write(String::from("\0")),
-    ]
+    [MoveTo(x, y),
+     ApplyStyle(Style::Reset),
+     Write(String::from("\0"))]
 }
 
 impl<T: Write> Renderer for Terminal<T> {
