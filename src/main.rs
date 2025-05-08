2c158c4dfbf9f15a86a474cfe74169155ee5bbee
ph <phpellerin@gmail.com>
2025-05-08 14:43:17 -0400

    bug(ui): Use 8bits color code for developping
    We will have support for `true color` and 8 bit color terminal.

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,6 @@ extern crate libc;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let w = window_size()?;
-    println!("window size: {:?}", w);
-
     let fb = Framebuffer::new(w.cols, w.rows);
     let mut fb_1 = fb.clone();
 
@@ -19,18 +17,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         2,
         3,
         Cell::Filled {
-            character: 'P',
-            foreground: fg(Color::Magenta),
-            background: bg(Color::Cyan),
+            character: 'K',
+            foreground: fg(Color::White),
+            background: bg(Color::Red),
         },
     );
+
     fb_1.set(
         2,
         4,
         Cell::Filled {
-            character: 'H',
-            foreground: fg(Color::Red),
-            background: bg(Color::Black),
+            character: 'k',
+            foreground: fg(Color::Black),
+            background: bg(Color::Cyan),
         },
     );
     fb_1.set(
@@ -49,6 +48,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _ = terminal::configure(&mut out);
     let mut renderer = Terminal::new(&mut out);
     renderer.submit(changesets);
+
+    // println!("\x1b[45mHoooko\x1b[0m");
+    // println!("\x1b[35mHoooko\x1b[0m");
 
     Ok(())
 }
